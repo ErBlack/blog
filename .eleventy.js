14fdef574ebed666aeed5af15d9425a1c6f2625f
erblack <erblack@practicum.com>
2023-04-12 09:28:44 +0200

    sketch

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,8 @@ const Image = require('@11ty/eleventy-img');
 const { dirname } = require('path');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addShortcode('image', async function (src, alt, sizes) {
-        console.log(dirname(this.page.inputPath));
-
-        const metadata = await Image(`${dirname(this.page.inputPath)}/${src}`, {
+    eleventyConfig.addShortcode('image', async function (src, alt, inputPath = this.page.inputPath) {
+        const metadata = await Image(`${dirname(inputPath)}/${src}`, {
             widths: [800],
             formats: ['avif', 'webp', 'jpeg'],
             outputDir: '_site/img',
@@ -13,7 +11,6 @@ module.exports = function (eleventyConfig) {
 
         const imageAttributes = {
             alt,
-            sizes,
             loading: 'lazy',
             decoding: 'async',
         };
--- posts/1/index.md
+++ posts/1/index.md
@@ -1,8 +1,11 @@
 ---
 title: Hello
 date: 2023-06-24
+tags:
+    - sketch
+layout: sketch.liquid
+images:
+    - 1.jpeg
 ---
 
 Hi! My name is Artem)
-
-{% image "1.jpeg", "Sketch with little me" %}
