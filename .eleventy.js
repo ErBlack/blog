1716cbdff15164e1ccd63e9d00d12562ed9a6267
erblack <erblack@practicum.com>
2023-04-14 01:05:41 +0200

    images automation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const Image = require('@11ty/eleventy-img');
 const { dirname } = require('path');
+const postImages = require('./lib/shortcodes/postImages.js');
+const sketch = require('./lib/shortcodes/sketch.js');
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addShortcode('image', async function (src, alt, inputPath = this.page.inputPath) {
         const metadata = await Image(`${dirname(inputPath)}/${src}`, {
-            widths: [800],
+            widths: [800, 1200],
             formats: ['avif', 'webp', 'jpeg'],
             outputDir: '_site/blog/img',
             urlPath: '/blog/img/',
@@ -14,8 +16,12 @@ module.exports = function (eleventyConfig) {
             alt,
             loading: 'lazy',
             decoding: 'async',
+            sizes: 800,
         };
 
-        return Image.generateHTML(metadata, imageAttributes);
+        return `<a>${Image.generateHTML(metadata, imageAttributes)}</a>`;
     });
+
+    eleventyConfig.addShortcode('postImages', postImages);
+    eleventyConfig.addShortcode('sketch', sketch);
 };
--- blog/posts/1/index.md
+++ blog/posts/1/index.md
@@ -4,8 +4,6 @@ date: 2023-06-24
 tags:
     - sketch
 layout: sketch.liquid
-images:
-    - 1.jpeg
 ---
 
 Hi! My name is Artem)
